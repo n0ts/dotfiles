130a512296a291fc1fc5ed8388f5468709379a80
n0ts <me@n0ts.org>
2013-11-06 09:30:13 +0900

    Added support solarized

--- source-highlight/src-hilite-lesspipe.sh
+++ source-highlight/src-hilite-lesspipe.sh
@@ -1,12 +1,23 @@
 #! /bin/sh
 
+STYLE_FILE=
+if [ -d "$HOME/.source-highlight-solarized" ]; then
+    STYLE_FILE="$HOME/.source-highlight-solarized/esc-solarized.style"
+else
+    STYLE_FILE="$HOME/source-highlight/wombat.style"
+fi
+
+if [ -z "$STYLE_FILE" ]; then
+  exit
+fi
+
 for source in "$@"; do
     case $source in
     *ChangeLog|*changelog) 
-        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$HOME/.env/source-highlight/wombat.style -i "$source" ;;
+        source-highlight --failsafe -f esc --lang-def=changelog.lang --style-file=$STYLE_FILE -i "$source" ;;
     *Makefile|*makefile) 
-        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$HOME/.env/source-highlight/wombat.style -i "$source" ;;
+        source-highlight --failsafe -f esc --lang-def=makefile.lang --style-file=$STYLE_FILE -i "$source" ;;
     *)
-        source-highlight --failsafe --infer-lang -f esc --style-file=$HOME/.env/source-highlight/wombat.style -i "$source" ;;
+        source-highlight --failsafe --infer-lang -f esc --style-file=$STYLE_FILE -i "$source" ;;
     esac
 done
