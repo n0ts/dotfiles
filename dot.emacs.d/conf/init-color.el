503d424d9e6fe4fc302947fbb824faddf5bca897
n0ts <me@n0ts.org>
2017-07-20 22:37:50 +0900

    Change emacs mode-line

--- dot.emacs.d/conf/init-color.el
+++ dot.emacs.d/conf/init-color.el
@@ -2,8 +2,8 @@
 
 (global-font-lock-mode t)
 
-(set-face-background 'modeline "gainsboro")
-(set-face-foreground 'modeline "black")
+(set-face-background 'mode-line "gainsboro")
+(set-face-foreground 'mode-line "black")
 (set-face-background 'highlight "gray10")
 (set-face-foreground 'highlight "tomato")
 (set-face-background 'scroll-bar "gray30")
