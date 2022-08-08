cb9c4d6ea15f81b1519bb45823f9d3f7f3e64198
n0ts <naoya.n@gmail.com>
2022-08-08 19:29:42 +0900

    Delete gcloud.zsh

--- dot.zsh.sources/gcloud.zsh
+++
@@ -1,9 +0,0 @@
-GCLOUD_DIR=/opt/google-cloud-sdk 
-if [ -d "$GCLOUD_DIR" ]; then
-  autoload -U compinit; compinit -u;
-
-  source $GCLOUD_DIR/path.zsh.inc
-  source $GCLOUD_DIR/completion.zsh.inc
-fi
-
-alias gcloud="pwd=`pwd`; cd $GCLOUD_DIR/bin; gcloud; cd $pwd"
